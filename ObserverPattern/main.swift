af18f3630f70b98d25423746cd363c92ce4d8129
Görkem Oktay <gorkem.oktay@masomo.com>
2019-02-19 12:10:39 +0300

    Initial Commit

---
+++ ObserverPattern/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObserverPattern
+//
+//  Created by Görkem Oktay on 19.02.2019.
+//  Copyright © 2019 Görkem Oktay. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
